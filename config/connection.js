ad7b8957d34ee841428efafe42a6f6ce61b3f4c3
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-22 23:16:59 -0400

    used the environment vairable

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require(`dotenv`).config();
 
 
 // create connection to our database
-const sequelize = new Sequelize('just_tech_news_db','root','Sjeet2bang2000oct//QL', {
+const sequelize = new Sequelize(proces.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, {
    host:'localhost',
    dialect:'mysql',
    port:3306
