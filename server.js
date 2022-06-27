321642cc5fba948a220e13a9b0d8643e20c0ffc4
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-26 23:56:31 -0400

    added login and logout function

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const path = require('path');
 const exhbs = require('express-handlebars');
 const hbs = exhbs.create({});
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookies: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 
 const app = express();
@@ -15,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
