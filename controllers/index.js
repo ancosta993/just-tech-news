85b03b681396e8025096289405aef25c2b01c52b
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-29 00:35:23 -0400

    added dashbord route and rendered the page

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
+const dashBoardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashBoardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
